c233c69b6b972ba0ce065f1f3f7e289b606f3492
adrianaVilar <adriana.correa@outlook.com>
2022-03-31 13:16:49 -0300

    Adicionados arquivos complementares e Desafio dos Dias da Semana

---
+++ udemy/controle/DesafioDiasDaSemana.java
@@ -0,0 +1,35 @@
+package controle;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class DesafioDiasDaSemana {
+    public static void main(String[] args) {
+        System.out.println("Digite um dia da semana para descobrir o seu número correspondente: ");
+
+        Scanner scan = new Scanner(System.in);
+
+        String dia = scan.next();
+
+        if ("domingo".equalsIgnoreCase(dia)) {
+            System.out.println("1");
+        } else if ("segunda".equalsIgnoreCase(dia)) {
+            System.out.println("2");
+        } else if ("terça".equalsIgnoreCase(dia)) {
+            System.out.println("3");
+        } else if ("quarta".equalsIgnoreCase(dia)) {
+            System.out.println("4");
+        } else if ("quinta".equalsIgnoreCase(dia)) {
+            System.out.println("5");
+        } else if ("sexta".equalsIgnoreCase(dia)) {
+            System.out.println("6");
+        } else if ("sábado".equalsIgnoreCase(dia)) {
+            System.out.println("7");
+        } else {
+            System.out.println("Digite um dia válido.");
+        }
+        System.out.println("Fim.");
+
+        scan.close();
+    }
+}
