d3b4ec3b59a1c447f3443ef25f900e6cc51be579
adrianaVilar <adriana.correa@outlook.com>
2022-03-31 19:19:40 -0300

    Incluídas opções de ç e acento nos nomes da semana

--- udemy/controle/DesafioDiasDaSemana.java
+++ udemy/controle/DesafioDiasDaSemana.java
@@ -14,7 +14,7 @@ public class DesafioDiasDaSemana {
             System.out.println("1");
         } else if ("segunda".equalsIgnoreCase(dia)) {
             System.out.println("2");
-        } else if ("terça".equalsIgnoreCase(dia)) {
+        } else if ("terça".equalsIgnoreCase(dia) || "terca".equalsIgnoreCase(dia)) {
             System.out.println("3");
         } else if ("quarta".equalsIgnoreCase(dia)) {
             System.out.println("4");
@@ -22,7 +22,7 @@ public class DesafioDiasDaSemana {
             System.out.println("5");
         } else if ("sexta".equalsIgnoreCase(dia)) {
             System.out.println("6");
-        } else if ("sábado".equalsIgnoreCase(dia)) {
+        } else if ("sábado".equalsIgnoreCase(dia) || "sabado".equalsIgnoreCase(dia)) {
             System.out.println("7");
         } else {
             System.out.println("Digite um dia válido.");
