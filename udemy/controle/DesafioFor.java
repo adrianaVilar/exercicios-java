86cc3ea4fd16998b93d611a2ae4c10576af3882a
adrianaVilar <adriana.correa@outlook.com>
2022-04-01 18:21:53 -0300

    Adicionada versao do desafio com while

--- udemy/controle/DesafioFor.java
+++ udemy/controle/DesafioFor.java
@@ -3,6 +3,13 @@ package controle;
 public class DesafioFor {
     public static void main(String[] args) {
 
+        int contador2 = 10;
+        while (contador2 >= 0) {
+            System.out.println(contador2);
+            contador2 -= 2;
+        }
+
+
         for (int contador = 10; contador >= 0; contador -= 2) {
             System.out.printf("Contador: %d\n", contador);
         }
