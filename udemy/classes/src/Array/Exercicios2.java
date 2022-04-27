77abbc5eec37d014d414e2db88998ff7f3da1482
adrianaVilar <adriana.correa@outlook.com>
2022-04-27 00:11:48 -0300

    Exercícios foreach

---
+++ udemy/classes/src/Array/Exercicios2.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Exercicios2 {
+
+    public static void main(String[] args) {
+
+        double[] notasAlunoA = {8.5, 8.4, 9.7};
+
+        for (double nota : notasAlunoA) {
+            System.out.print(nota + " ");
+        }
+    }
+}
---
+++ udemy/classes/src/Array/Foreach.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Foreach {
+
+    public static void main (String [] args){
+
+        double[] notas = {9.8, 9.5, 9.7, 10};
+
+        for(double nota: notas) {
+            System.out.print(nota + " ");
+        }
+    }
+}
