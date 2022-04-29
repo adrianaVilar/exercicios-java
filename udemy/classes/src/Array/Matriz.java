05635b0e962225e5fce2e7378a3f7c209c225029
adrianaVilar <adriana.correa@outlook.com>
2022-04-28 21:47:08 -0300

    Adicionado exercícios de matrizes

---
+++ udemy/classes/src/Array/Matriz.java
@@ -0,0 +1,37 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Matriz {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite a quantidade de alunos da turma: ");
+        int qtdeAlunos = scan.nextInt();
+        System.out.println("Digite a quantidade de notas: ");
+        int qtdeNotas = scan.nextInt();
+
+        double[][] notasDosAlunos = new double[qtdeAlunos][qtdeNotas];
+
+        double soma = 0;
+
+        for (int a = 0; a < notasDosAlunos.length; a++) {
+            for (int n = 0; n < notasDosAlunos[a].length; n++) {
+                System.out.printf("Digite a nota %d do aluno %d: ", (n + 1), (a + 1));
+                notasDosAlunos[a][n] = scan.nextDouble();
+                soma += notasDosAlunos[a][n];
+            }
+        }
+
+        double media = soma / (qtdeAlunos * qtdeNotas);
+        System.out.printf("Média: %.2f\n", media);
+
+        for(double[] notasDaTurma : notasDosAlunos) {
+            System.out.println(Arrays.toString(notasDaTurma));
+        }
+
+        scan.close();
+    }
+}
