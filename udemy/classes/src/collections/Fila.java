a633595d02fdab3af09d8cc06bf269b1e046ffd9
adrianaVilar <adriana.correa@outlook.com>
2022-05-04 16:14:56 -0300

    Criada classe Fila

--- udemy/classes/src/collections/Fila.java
+++ udemy/classes/src/collections/Fila.java
@@ -1,17 +1,31 @@
 package collections;
 
-import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Queue;
 
-public class Queue {
+public class Fila {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
         Queue<String> fila = new LinkedList<>();
 
         fila.add("Adri");
-        
+        fila.offer("Érick");
+        System.out.println("Fila: " + fila);
 
+        System.out.println("Primeiro elemento: " + fila.peek());
+        System.out.println("Primeiro elemento: " + fila.element());
+
+        System.out.println("Tamanho: " + fila.size());
+        System.out.println("Se está vazia: " + fila.isEmpty());
+        //fila.clear();
+        System.out.println("Fila: " + fila);
+
+        System.out.println("Remover: " + fila.poll());
+        System.out.println("Remover: " + fila.remove());
+        System.out.println("Remover: " + fila.poll());
+        System.out.println("Fila: " + fila);
+        
     }
 
 }
