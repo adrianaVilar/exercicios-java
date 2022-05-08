f9cbaf3f8aba00b865fbac1e0285899a77e36053
adrianaVilar <adriana.correa@outlook.com>
2022-05-08 03:00:34 -0300

    Calculadora para casais e exercicio Ano bissexto

--- udemy/classes/src/collections/CalculadoraAnoBissexto.java
+++ udemy/classes/src/collections/CalculadoraAnoBissexto.java
@@ -1,2 +1,23 @@
-package collections;public class CalculadoraAnoBissexto {
+package collections;
+
+import java.util.Scanner;
+
+public class CalculadoraAnoBissexto {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite que ano deseja verificar se é ou não bissexto: ");
+
+        int ano = scan.nextInt();
+
+        if (ano % 400 == 0 || (ano % 4 == 0 && ano % 100 != 0)) {
+            System.out.println("O ano é bissexto.");
+        } else {
+            System.out.println("O ano não é bissexto.");
+        }
+        scan.close();
+    }
 }
+
