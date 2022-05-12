2111cd6fca3500b1b4e5700eaf1c6c737b22b517
adrianaVilar <adriana.correa@outlook.com>
2022-05-12 14:03:47 -0300

    Exercicio da raiz quadrada e documentos: exercicios módulos 4 e listagem de métodos estáticos da API

--- moodleifrs/src/ferramentas/RaizQuadrada.java
+++ moodleifrs/src/ferramentas/RaizQuadrada.java
@@ -1,7 +1,12 @@
 package ferramentas;
 
-public class Quadrado {
+public class RaizQuadrada {
 
-    
+    public static void main(String[] args) {
 
+        for (int i = 1; i <= 100; i++) {
+            double raizQuadrada = Math.sqrt(i);
+            System.out.printf("A raiz quadrada de %d é %.2f\n", i, raizQuadrada);
+        }
+    }
 }
