3fb9b891ad2500d1fdf4e4770ba1607f412f71bb
adrianaVilar <adriana.correa@outlook.com>
2022-05-11 22:39:22 -0300

    Adicionado tutorial sobre comandos do GitBash e exercício sobre o consumo de combustivel

--- moodleifrs/src/estruturascontrole/ConsumoDoCarro.java
+++ moodleifrs/src/estruturascontrole/ConsumoDoCarro.java
@@ -1,2 +1,34 @@
-package estruturascontrole;public class ConsumoDoCarro {
+package estruturascontrole;
+
+import java.util.Scanner;
+
+public class ConsumoDoCarro {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite quantas viagens foram realizadas: ");
+
+        int qtdViagens = scan.nextInt();
+        double totalLitros = 0;
+        double totalKm = 0;
+        double media;
+
+        for (int i = 0; i < qtdViagens; i++){
+            System.out.println("Digite quantos litros foram gastos durante a " + (i+1) + "a viagem: ");
+            double litros = scan.nextDouble();
+            totalLitros += litros;
+            System.out.println("Digite a quantidade de km rodados na " + (i+1) + "a viagem: ");
+            double km = scan.nextDouble();
+            totalKm += km;
+        }
+
+        media = totalKm / totalLitros;
+
+        System.out.printf("Média: %.2f km/L.", media);
+
+        scan.close();
+    }
+
 }
