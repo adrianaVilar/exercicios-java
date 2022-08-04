ececa9853450402c0693ad815efddaef5feee649
Adriana Vilar <adriana.correa@outlook.com>
2022-08-04 12:30:32 -0300

    projeto calculadora

---
+++ moodleifrs/src/ferramentas/calculadora.java
@@ -0,0 +1,14 @@
+package ferramentas;
+
+public class calculadora {
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = Calcula(a + 1);
+
+        public Calcula(a){
+            int c;
+            c = a+10;
+        }
+    }
+}
