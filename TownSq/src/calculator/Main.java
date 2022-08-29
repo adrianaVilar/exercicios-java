8ccc1f1ec7b3b0b638908b3f0415ce21b4281b2f
Adriana Vilar <adriana.correa@outlook.com>
2022-08-29 14:52:42 -0300

    Atualizada String mostrada no terminal

--- TownSq/src/calculator/Main.java
+++ TownSq/src/calculator/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] flavors = {1, 1, 1, 2, 2, 2, 0, 0, 0, 1};
+        int[] flavors = {1, 1, 1, 2, 2, 2, 0, 0, 0};
 
-        System.out.println("Valor que mais se repete: " + Calculator.getFrequency(flavors));
+        System.out.println("O código do sabor é: " + Calculator.getFrequency(flavors));
 
     }
 }
