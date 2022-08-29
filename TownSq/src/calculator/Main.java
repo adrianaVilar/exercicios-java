cc6a4c76cd480a03a4374da15c34ccded112c877
Adriana Vilar <adriana.correa@outlook.com>
2022-08-29 12:53:33 -0300

    Teste com array diferente

--- TownSq/src/calculator/Main.java
+++ TownSq/src/calculator/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] flavors = {1, 1, 1, 2, 2, 2, 0, 0, 0};
+        int[] flavors = {1, 1, 1, 2, 2, 2, 0, 0, 0, 1};
 
         System.out.println("Valor que mais se repete: " + Calculator.getFrequency(flavors));
 
